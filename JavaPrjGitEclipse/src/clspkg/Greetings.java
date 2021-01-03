d82441bd1fee3dc85d05ed6000a8a367fcd67c4a
dam <dam@dam-virtual-machine>
2021-01-03 03:23:40 +0000

    new now

---
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -0,0 +1,10 @@
+package clspkg;
+
+public class Greetings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Greetings LYIT Student L0092054");
+	}
+
+}
