b89fa523c8c184ff391fba84799f46ce1b7746e0
Damien Duffy <57752037+L00092054@users.noreply.github.com>
2021-01-03 03:33:17 +0000

    Update Greetings.java
    second System.out.println() statement was added.

--- JavaPrjGitEclipse/src/clspkg/Greetings.java
+++ JavaPrjGitEclipse/src/clspkg/Greetings.java
@@ -5,6 +5,8 @@ public class Greetings {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("Greetings LYIT Student L0092054");
+		// Second print line
+		System.out.print("Greetings LYIT Student L0092054");
 	}
 
 }
